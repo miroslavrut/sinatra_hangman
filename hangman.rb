8629b7062db29703974eaac29098134d7170f024
morp <rutesicm27@gmail.com>
2019-09-22 19:02:09 +0200

    Read from file

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
 get '/' do
-  p "hangman"
+  erb :index
+end
+
+
+get '/newgame' do
+  # set_variables
+  redirect '/'
+end
+
+def read_from_file
+  words = Array.new
+  File.open('5desk.txt', 'r') do |f|
+    f.each_line do |line|
+      line.gsub!(/[^a-zA-Z]/, "")
+      if line.length >= 5 && line.length <= 12
+        words << line.downcase
+      end
+    end
+  end
+  words
 end
