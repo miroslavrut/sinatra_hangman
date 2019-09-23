d6f01afc1ce86a53245f920942501036a8332ff3
morp <rutesicm27@gmail.com>
2019-09-23 22:15:18 +0200

    Add session

--- hangman.rb
+++ hangman.rb
@@ -1,27 +1,45 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+enable :sessions
 
 
 get '/' do
+  @secret_word = session[:secret_word]
+  @display = session[:display]
+  @missed_letters = session[:missed_letters]
   erb :index
 end
 
-
 get '/newgame' do
-  # set_variables
+  set_session_variables
   redirect '/'
 end
 
-def read_from_file
-  words = Array.new
-  File.open('5desk.txt', 'r') do |f|
-    f.each_line do |line|
-      line.gsub!(/[^a-zA-Z]/, "")
-      if line.length >= 5 && line.length <= 12
-        words << line.downcase
+post '/' do
+  # check_guess(params["guess"].downcase)
+  redirect '/'
+end
+
+helpers do
+  def set_session_variables
+    session[:secret_word] = read_from_file.sample
+    session[:turns] = 7
+    session[:display] = Array.new(session[:secret_word].length, "_")
+    session[:missed_letters] = Array.new
+  end
+
+  def read_from_file
+    words = Array.new
+    File.open('5desk.txt', 'r') do |f|
+      f.each_line do |line|
+        line.gsub!(/[^a-zA-Z]/, "")
+        if line.length >= 5 && line.length <= 12
+          words << line.downcase
+        end
       end
     end
+    words
   end
-  words
+
 end
